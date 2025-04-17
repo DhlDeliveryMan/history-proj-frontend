ee2de0264816f885b4bc7b9ccba781b928844343
Olivers Vītiņš <o.vitins@resultium.io>
2025-04-17 16:55:36 +0300

    Fix authentication page bugs

--- app/auth/authForm.tsx
+++ app/auth/authForm.tsx
@@ -13,6 +13,7 @@ const login = async (e: any, navigate: NavigateFunction) => {
   const formJson = Object.fromEntries(formData.entries());
 
   let response;
+
   try {
     response = await axios.post(
       `${import.meta.env.VITE_BACKEND_URL}/api/auth/login`,
@@ -49,6 +50,53 @@ const login = async (e: any, navigate: NavigateFunction) => {
   }
 };
 
+const register = async (e: any, navigate: NavigateFunction) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+
+  const formJson = Object.fromEntries(formData.entries());
+
+  let response;
+
+  try {
+    response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/api/auth/register`,
+      {
+        username: formJson.username,
+        email: formJson.email,
+        password: formJson.password,
+      },
+      { withCredentials: true }
+    );
+  } catch (error: any) {
+    switch (error.status) {
+      case 400:
+        toast.error("Nepareizi ievadīti dati!", {});
+        break;
+      case 403:
+        toast.error("Jums nav piekļuves šai darbībai!", {});
+        break;
+      case 404:
+        toast.error("Šāds lietotājs jau pastāv!", {});
+        break;
+      case 500:
+        toast.error("Servera kļūda. Lūdzu, mēģiniet vēlreiz vēlāk.", {});
+        break;
+      default:
+        toast.error("Nezināma kļūda. Lūdzu, mēģiniet vēlreiz.", {});
+        break;
+    }
+    return;
+  }
+
+  if (response && response.status === 201) {
+    toast.success("Reģistrācija veiksmīga!", {});
+    navigate("/tests");
+  }
+};
+
 const AuthForm: React.FC = () => {
   const [isRegistering, setIsRegistering] = React.useState(false);
 
@@ -58,23 +106,26 @@ const AuthForm: React.FC = () => {
     <div className="mt-8 p-6 bg-white shadow rounded-lg max-w-sm w-full text-center relative overflow-hidden px-4 min-h-[300px]">
       <div
         className={`absolute inset-0 transition-transform duration-500 ${
-          isRegistering ? "-translate-x-full" : "translate-x-0"
+          isRegistering ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {/* Login Form */}
-        <form onSubmit={(e) => login(e, navigate)} className="w-full mt-10">
+        {/* Registration Form */}
+        <form onSubmit={(e) => register(e, navigate)} className="w-full mt-10">
           <input
             type="text"
+            name="username"
             placeholder="Lietotājvārds"
             className="border p-2 mb-4 w-10/12 max-w-sm rounded text-center"
           />
           <input
             type="email"
+            name="email"
             placeholder="e-pasts"
             className="border p-2 mb-4 w-10/12 max-w-sm rounded text-center"
           />
           <input
             type="password"
+            name="password"
             placeholder="Parole"
             className="border p-2 mb-4 w-10/12 max-w-sm rounded text-center"
           />
@@ -87,7 +138,7 @@ const AuthForm: React.FC = () => {
           <p className="mt-4 text-sm text-gray-700 text-center">
             Ienākt?{" "}
             <span
-              onClick={() => setIsRegistering(true)}
+              onClick={() => setIsRegistering(false)}
               className="text-emerald-800 cursor-pointer hover:underline"
             >
               Klikšķiniet šeit
@@ -98,10 +149,10 @@ const AuthForm: React.FC = () => {
 
       <div
         className={`absolute inset-0 transition-transform duration-500 ${
-          isRegistering ? "translate-x-0" : "translate-x-full"
+          isRegistering ? "translate-x-full" : "translate-x-0"
         }`}
       >
-        {/* Registration Form */}
+        {/* Login Form */}
         <form
           onSubmit={(e) => login(e, navigate)}
           className="w-full flex flex-col items-center mt-10"
@@ -127,7 +178,7 @@ const AuthForm: React.FC = () => {
           <p className="mt-4 text-sm text-gray-700 text-center">
             Atpakaļ uz reģistrāciju?{" "}
             <span
-              onClick={() => setIsRegistering(false)}
+              onClick={() => setIsRegistering(true)}
               className="text-emerald-800 cursor-pointer hover:underline"
             >
               Klikšķiniet šeit
